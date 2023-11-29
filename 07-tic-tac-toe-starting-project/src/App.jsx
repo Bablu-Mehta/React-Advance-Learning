1311e64ec86a6414ae9fdb5bfe2edfac48dcea0d
Bablu Mehta <75409318+Bablu-Mehta@users.noreply.github.com>
2023-11-29 11:10:17 +0000

    Rendring Multi-Dimensional List (Error:The styling for GameBoard Component isn't Coming need to resolve it.

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,15 +1,16 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
-
+import "./index.css";
 
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-         <Player initialName="Player-1" symbol='X' />
-         <Player initialName="Player-2" symbol='O' />
+          <Player initialName="Player-1" symbol="X" />
+          <Player initialName="Player-2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
