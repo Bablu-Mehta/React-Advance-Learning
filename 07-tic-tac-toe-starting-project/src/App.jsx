e5926b2d75db86c32bf088d9b70a2df3fcf34039
Bablu Mehta <babloomehta895012@gmail.com>
2023-12-06 06:20:17 +0530

    Deriving Computed Values From Other Computed Values

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -39,11 +39,19 @@ for(const turn of gameTurns)
   gameBoard[row][col] = player;
 }
 
-  for(combination of WINNING_COMBINATIONS)
+  let winner;
+
+  for(const combination of WINNING_COMBINATIONS)
   {
-    const firstSquareSymbol = gameBoard[]
-    const secondSquareSymbol
-    const thirdSquareSymbol
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+    console.log(firstSquareSymbol);
+   // console.log(winner);
+    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+      winner = firstSquareSymbol;
+    }
+   // console.log(winner);
   }
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -63,6 +71,7 @@ for(const turn of gameTurns)
           <Player initialName="Player-1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player-2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
+        {winner && <p>You Won, { winner }!</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
