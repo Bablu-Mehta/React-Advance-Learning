c368d35f6ab2b91e6474553785045d08998c6809
Bablu Mehta <babloomehta895012@gmail.com>
2023-12-14 10:08:37 +0530

    Adding Challenges to the Demo Project

--- Refs Portals/01-starting-project/src/App.jsx
+++ Refs Portals/01-starting-project/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenger from './components/TimerChallenger.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenger title="Easy" targetTime={1} />
+        <TimerChallenger title="Not Easy" targetTime={5} />
+        <TimerChallenger title="Getting Tough" targetTime={10} />
+        <TimerChallenger title="pros only" targetTime={15} />
+      </div>
     </>
   );
 }
