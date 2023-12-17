fa56da1093f990d1ffa52c5e844f0226168c458d
Bablu Mehta <babloomehta895012@gmail.com>
2023-12-17 17:35:05 +0530

    Validating User Input & Showing an Error Modal via useImerativeHandle

--- Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
+++ Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
@@ -1,45 +1,64 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewPorject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
 
-  function handleSave(){
+  function handleSave() {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    //validation..
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
-        title:enteredTitle,
-        description:enteredDescription,
-        dueDate:enteredDueDate
-    })
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end my-4 gap-4">
-        <li>
-          <button className="text-stone=800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 py-2 px-6 rounded-md"
-          onClick={handleSave}>
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2>Invalid Input</h2>
+        <p>Oops .... looks like you forgot to enter a value</p>
+        <p>Pleade Make sure you provide a Valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end my-4 gap-4">
+          <li>
+            <button className="text-stone=800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 py-2 px-6 rounded-md"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
