fcf64459d7a4a206038a7229ffb725bd97daef5a
Bablu Mehta <babloomehta895012@gmail.com>
2023-12-17 16:39:49 +0530

    Collecting User Input with Refs & Forwarded Refs

--- Practice Project - Project Management/01-starting-project/src/App.jsx
+++ Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -18,10 +18,25 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData){
+    setProjectsState(prevState =>{
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return{
+        ...prevState,
+        project:[...prevState.project, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState);
+  
   let content;
 
   if(projectsState.selectedProjectId === null){
-    content = <NewPorject />
+    content = <NewPorject onAdd={handleAddProject} />
   } else if(projectsState.selectedProjectId === undefined){
     content = <NoProjectSelected onStartAddProject = {handleStartAddProject} />
   }
--- Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
+++ Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
@@ -1,17 +1,45 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function NewPorject(){
-    return(
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end my-4 gap-4">
-                <li><button className="text-stone=800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="bg-stone-800 text-stone-50 hover:bg-stone-950 py-2 px-6 rounded-md">Save</button></li>
-            </menu>
-            <div>
-               <Input label="Title" />
-               <Input label="Description" textarea />
-               <Input label="Due Date" />
-            </div>
-        </div>
-    )
+export default function NewPorject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave(){
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    //validation..
+
+    onAdd({
+        title:enteredTitle,
+        description:enteredDescription,
+        dueDate:enteredDueDate
+    })
+  }
+
+  return (
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end my-4 gap-4">
+        <li>
+          <button className="text-stone=800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button className="bg-stone-800 text-stone-50 hover:bg-stone-950 py-2 px-6 rounded-md"
+          onClick={handleSave}>
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due Date" />
+      </div>
+    </div>
+  );
 }
