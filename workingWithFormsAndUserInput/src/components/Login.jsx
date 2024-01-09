1b601fb4f2705865b3dc40a0f1e7ae0d3419a91e
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-09 18:21:52 +0530

    Validating Input Upon Form Submission

--- workingWithFormsAndUserInput/src/components/Login.jsx
+++ workingWithFormsAndUserInput/src/components/Login.jsx
@@ -1,16 +1,25 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("submmited");
 
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
-    console.log(enteredEmail, enteredPassword);
+
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
+
+    console.log("Sending HTTP Request...!");
   }
 
   return (
@@ -21,6 +30,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>PLease Enter a Valid Email</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
