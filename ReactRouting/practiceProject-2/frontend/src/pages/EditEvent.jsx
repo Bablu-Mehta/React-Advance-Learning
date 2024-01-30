c156144cdb471133ee881b37706a8f1d1729cba0
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-30 11:19:54 +0530

    The useRouteLoaderData() Hook & Accessing Data From Other Routes

--- ReactRouting/practiceProject-2/frontend/src/pages/EditEvent.jsx
+++ ReactRouting/practiceProject-2/frontend/src/pages/EditEvent.jsx
@@ -1,5 +1,9 @@
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 function EditEventPage() {
-  return <h1>THis is the edit event page.</h1>;
+  const data = useRouteLoaderData("event-detail");
+  return <EventForm event={data.event} />;
 }
 
 export default EditEventPage;
