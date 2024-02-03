06fd84d80bdace5e4768173391d9de47d24e8ae5
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-03 14:35:46 +0530

    Introducing Component Components

--- ReactPatternsAndBestPractices/src/App.jsx
+++ ReactPatternsAndBestPractices/src/App.jsx
@@ -1,5 +1,40 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why Work With us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            className="accordion-item"
+            title="We got 20 years of Experience."
+          >
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                we are in the business of planning highly individualized
+                vacation trips for more than 20 Years.{" "}
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            className="accordion-item"
+            title="We are working with local guides."
+          >
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                we are in the business of planning highly individualized
+                vacation trips for more than 20 Years.{" "}
+              </p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
