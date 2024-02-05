c2427db155c2fcd28467e301a699910f561b66ed
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-05 19:59:33 +0530

    hanving some bugs in using the custome store

--- ReplacingReduxWithHooks/src/components/Products/ProductItem.js
+++ ReplacingReduxWithHooks/src/components/Products/ProductItem.js
@@ -1,16 +1,16 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import Card from "../UI/Card";
 import "./ProductItem.css";
+import { useStore } from "../../hooks-store/store";
 // import { toggleFav } from '../../store/actions/products';
-import { ProductsContext } from "../../context/products-context";
 
 const ProductItem = (props) => {
-  const toggleFav = useContext(ProductsContext).toggleFav;
-
+  const dispatch = useStore()[1];
   const toggleFavHandler = () => {
     // dispatch(toggleFav(props.id));
-    toggleFav(props.id);
+    // toggleFav(props.id);
+    dispatch("TOGGLE_FAV", props.id);
   };
 
   return (
