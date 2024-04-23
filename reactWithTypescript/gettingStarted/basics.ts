1d93d3a1bf682f92e3300a9fd5bbca9532a368ca
Bablu Mehta <babloomehta895012@gmail.com>
2024-04-23 08:54:39 +0530

    working with arrays and object types

---
+++ reactWithTypescript/gettingStarted/basics.ts
@@ -0,0 +1,29 @@
+//primitives
+
+let age: number = 24;
+
+age = 12;
+
+let username: string;
+
+username = "Bablu";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+//more complex types:
+
+let hobbies: string[];
+
+hobbies = ["chess", "cricket"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "bablu",
+  age: 24,
+};
